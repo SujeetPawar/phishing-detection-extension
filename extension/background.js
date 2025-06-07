9b2172142d51f7eb147a04c0a4d90af03a9c2c90
sujeet <sujeet17052003@gmail.com>
2025-06-07 15:07:33 +0530

    no need files deleted

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://phishing-detection-extension-2g6e.onrender.com";
 const BATCH_SIZE = 5;
 let currentResults = [];
 let processingQueue = [];
